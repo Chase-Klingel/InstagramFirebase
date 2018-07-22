8295c110422800337ced884b0d98bdd9eebd2634
Chase-Klingel <klingelchase@gmail.com>
2018-07-22 16:11:32 -0700

    fetch user info and display on profile header

--- InstagramFirebase/AppDelegate.swift
+++ InstagramFirebase/AppDelegate.swift
@@ -18,6 +18,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
         
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        window?.rootViewController = MainTabBarController()
+        
         return true
     }
 
---
+++ InstagramFirebase/MainTabBarController.swift
@@ -0,0 +1,29 @@
+//
+//  MainTabBarController.swift
+//  InstagramFirebase
+//
+//  Created by Chase Klingel on 7/22/18.
+//  Copyright © 2018 Chase Klingel. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let redVC = UIViewController()
+        redVC.view.backgroundColor = .red
+        
+        let layout = UICollectionViewFlowLayout()
+        let userProfileController = UserProfileController(collectionViewLayout: layout)
+        
+        let navController = UINavigationController(rootViewController: userProfileController)
+        navController.tabBarItem.image = #imageLiteral(resourceName: "profile_unselected")
+        navController.tabBarItem.selectedImage = #imageLiteral(resourceName: "profile_selected")
+        
+        tabBar.tintColor = .black
+        
+        viewControllers = [navController]
+    }
+}
