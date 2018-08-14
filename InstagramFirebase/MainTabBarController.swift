33feca1db171d39d9a1abcc7d0984318bace5a10
Chase-Klingel <klingelchase@gmail.com>
2018-08-13 17:55:04 -0700

    Setup photo selector skeleton

--- InstagramFirebase/MainTabBarController.swift
+++ InstagramFirebase/MainTabBarController.swift
@@ -9,10 +9,30 @@
 import UIKit
 import Firebase
 
-class MainTabBarController: UITabBarController {
+class MainTabBarController: UITabBarController,
+    UITabBarControllerDelegate {
+    
+    func tabBarController(_ tabBarController: UITabBarController,
+                          shouldSelect viewController: UIViewController) -> Bool {
+        
+        let index = viewControllers?.index(of: viewController)
+        if index == 2 {
+            let layout = UICollectionViewFlowLayout()
+            let photoSelectorController = PhotoSelectorController(collectionViewLayout: layout)
+            let navController = UINavigationController(rootViewController: photoSelectorController)
+            
+            present(navController, animated: true, completion: nil)
+            
+            return false
+        }
+        
+        return true
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.delegate = self
         
         if (Auth.auth().currentUser == nil) {
             /*
@@ -33,18 +53,17 @@ class MainTabBarController: UITabBarController {
     }
     
     func setupViewControllers() {
+        let layout = UICollectionViewFlowLayout()
+
         let homeNavController = templateNavController(unselectedImage: #imageLiteral(resourceName: "home_unselected"),
                                                       selectedImage: #imageLiteral(resourceName: "home_selected"),
-                                                      rootViewController: UserProfileController(collectionViewLayout: UICollectionViewFlowLayout()))
+                                                      rootViewController: UserProfileController(collectionViewLayout: layout))
         let searchNavController = templateNavController(unselectedImage: #imageLiteral(resourceName: "search_unselected"), selectedImage: #imageLiteral(resourceName: "search_selected"))
         let plusNavController = templateNavController(unselectedImage: #imageLiteral(resourceName: "plus_unselected"), selectedImage: #imageLiteral(resourceName: "plus_unselected"))
         let likeNavController = templateNavController(unselectedImage: #imageLiteral(resourceName: "like_unselected"), selectedImage: #imageLiteral(resourceName: "like_selected"))
-        
-        let layout = UICollectionViewFlowLayout()
-        let userProfileController = UserProfileController(collectionViewLayout: layout)
-        let userProfileNavController = UINavigationController(rootViewController: userProfileController)
-        userProfileNavController.tabBarItem.image = #imageLiteral(resourceName: "profile_unselected")
-        userProfileNavController.tabBarItem.selectedImage = #imageLiteral(resourceName: "profile_selected")
+        let userProfileNavController = templateNavController(unselectedImage: #imageLiteral(resourceName: "profile_unselected"),
+                                                             selectedImage: #imageLiteral(resourceName: "profile_selected"),
+                                                             rootViewController: UserProfileController(collectionViewLayout: layout))
         
         tabBar.tintColor = .black
         
